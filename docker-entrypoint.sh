3546210a617bfb8421e185468fbfcc24c86857ce
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2018-07-02 00:10:06 +0200

    Cleanup docker-entrypoint script * Cleanup variable naming * Fix shellcheck findings - globing * Rename setup done marker file to .init

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ set -o nounset
 
 mysql_root_password="/run/secrets/com.ragedunicorn.mysql.root_password"
 mysql_app_user="/run/secrets/com.ragedunicorn.mysql.app_user"
-mysql_app_user_password="/run/secrets/com.ragedunicorn.mysql.app_user_password"
+mysql_app_password="/run/secrets/com.ragedunicorn.mysql.app_user_password"
 
 function create_data_dir {
   echo "$(date) [INFO]: Creating data directory ${MYSQL_DATA_DIR} and setting permissions"
@@ -23,18 +23,18 @@ function create_run_dir {
 }
 
 function set_init_done {
-  touch "${MYSQL_DATA_DIR}"/setup.d
+  touch "${MYSQL_DATA_DIR}/.init"
   echo "$(date) [INFO]: Init script done"
 }
 
 function init {
-  if [ -f "${MYSQL_DATA_DIR}/setup.d" ]; then
+  if [ -f "${MYSQL_DATA_DIR}/.init" ]; then
     echo "$(date) [INFO]: Init script already run - starting MYSQL"
 
     # check if run directory exists
     create_run_dir
     # start mysql in foreground with base- and datadir set
-    exec gosu ${MYSQL_USER} /usr/bin/mysqld_safe --datadir="${MYSQL_DATA_DIR}"
+    exec gosu "${MYSQL_USER}" /usr/bin/mysqld_safe --datadir="${MYSQL_DATA_DIR}"
   else
     echo "$(date) [INFO]: First time setup - running init script"
     create_data_dir
@@ -45,18 +45,18 @@ function init {
       exit 1
     fi
 
-    if [ -f "${mysql_app_user}" ] && [ -f "${mysql_app_user_password}" ] && [ -f "${mysql_root_password}" ]; then
+    if [ -f "${mysql_app_user}" ] && [ -f "${mysql_app_password}" ] && [ -f "${mysql_root_password}" ]; then
       echo "$(date) [INFO]: Found docker secrets - using secrets to setup mysql"
 
       mysql_root_password="$(cat ${mysql_root_password})"
       mysql_app_user="$(cat ${mysql_app_user})"
-      mysql_app_user_password="$(cat ${mysql_app_user_password})"
+      mysql_app_password="$(cat ${mysql_app_password})"
     else
       echo "$(date) [INFO]: No docker secrets found - using environment variables"
 
-      mysql_root_password="${MYSQL_ROOT_PASSWORD}"
-      mysql_app_user="${MYSQL_APP_USER}"
-      mysql_app_user_password="${MYSQL_APP_PASSWORD}"
+      mysql_root_password="${MYSQL_ROOT_PASSWORD:?Missing environment variable MYSQL_ROOT_PASSWORD}"
+      mysql_app_user="${MYSQL_APP_USER:?Missing environment variable MYSQL_APP_USER}"
+      mysql_app_password="${MYSQL_APP_PASSWORD:?Missing environment variable MYSQL_APP_PASSWORD}"
     fi
 
     unset "${MYSQL_ROOT_PASSWORD}"
@@ -67,7 +67,7 @@ function init {
     # However that data is lost when the image is pulled and the container started. Because of
     # this the folder needs to be initialzed before proceeding. Note that the root password is set
     # at a later point
-    if [ -z "$(ls -A ${MYSQL_DATA_DIR})" ]; then
+    if [ -z "$(ls -A "${MYSQL_DATA_DIR}")" ]; then
        mysqld --initialize-insecure --datadir="${MYSQL_DATA_DIR}" --user="${MYSQL_USER}"
     fi
 
@@ -87,8 +87,6 @@ function init {
 
       echo "$(date) [INFO]: Waiting for confirmation of MYSQL service startup, trying ${i}/${LOOP_LIMIT}..."
       sleep 5
-
-      # use initial password of mysql
       mysql -uroot -e "status" > /dev/null 2>&1 && break
       i=$((i + 1))
     done
@@ -98,8 +96,8 @@ function init {
 
     # create new user and grant remote access
     echo "$(date) [INFO]: Creating new user ${mysql_app_user}"
-    sed -e "s/{{password}}/${mysql_app_user_password}/g" \
-      -e "s/{{user}}/${mysql_app_user}/g" /home/user.sql | mysql -uroot -p${mysql_root_password};
+    sed -e "s/{{password}}/${mysql_app_password}/g" \
+      -e "s/{{user}}/${mysql_app_user}/g" /home/user.sql | mysql -uroot -p"${mysql_root_password}";
 
     if [ $? -ne 0 ]; then
       echo "$(date) [ERROR]: Failed to create new user";
@@ -111,14 +109,14 @@ function init {
 
     echo "$(date) [INFO]: Finished database setup"
 
-    mysqladmin -uroot -p${mysql_root_password} shutdown
+    mysqladmin -uroot -p"${mysql_root_password}" shutdown
 
     sleep 5 # wait for mysql to shutdown
 
     set_init_done
 
     # start mysql in foreground with datadir set
-    exec gosu ${MYSQL_USER} /usr/bin/mysqld_safe --datadir="${MYSQL_DATA_DIR}"
+    exec gosu "${MYSQL_USER}" /usr/bin/mysqld_safe --datadir="${MYSQL_DATA_DIR}"
   fi
 }
 
